d4bc3a0f792faa6f8471ebebf4f49cbb19f8090b
Brian Russell <Brianrussell2017@yahoo.com>
2024-12-26 13:28:07 -0500

    Updates from last week

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 
 namespace PracticeProject
@@ -19,6 +20,11 @@ namespace PracticeProject
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        
+        DispatcherTimer timer;
+        
+        int startCount = 60;
+        int minCount = 0;
         string[] Story = {
             "True,", "Nervous,", "very,", "very", "dreadfully", "nervous", "I", "had", "been", "and", "am;",
             "but", "why", "will", "you", "say", "that", "I", "am", "mad?", "The", "disease", "had", "sharpened",
@@ -51,7 +57,20 @@ namespace PracticeProject
             "would", "a", "madman", "have", "been", "so", "wise", "as", "this?", "And", "then,", "when", "my", "head",
             "was", "well", "in", "the", "room,", "I", "undid", "the", "lantern", "cautiously", "—", "oh,", "so", "cautiously"
         };
-        string messageBoxText = "Game has finished";
+        private string _fullName;
+        public string FullName
+        {
+            get { return _fullName; }
+            set
+            {
+                if (_fullName != value)
+                {
+                    _fullName = value;
+                    OnPropertyChanged();
+                }
+
+            }
+        }
 
         private int _correct;
         public int Correct 
@@ -87,7 +106,13 @@ namespace PracticeProject
             DataContext = this;
             InitializeComponent();
             TextBlock.Text = "I will be a random word";
-            Textbox.Text="Press start and begin typing";
+            Textbox.Text = "Press start and begin typing";
+            TxtInterval.Text = "---";
+            lblCorrect.Content = "Correct " + _correct;
+            lblIncorrect.Content = "Incorrect " + _incorrect;
+            fullName.Text = "BEFORE STARTING!Please Enter Your Full Name In Here!(Use spaces if necessary)";
+            timer = new DispatcherTimer();
+           
 
         }
 
@@ -123,23 +148,71 @@ namespace PracticeProject
                 Textbox.Text = null;
 
             }
-            lblCorrect.Content = "Correct" + _correct;
+            lblCorrect.Content = "Correct " + _correct;
             lblIncorrect.Content="Incorrect "+ _incorrect;
         }
 
         private void StartClicked(object sender, RoutedEventArgs e)
         {
             Textbox.Focus();
+            
+
+            //  timer = new DispatcherTimer();
+            // timer.Interval = TimeSpan.FromSeconds(startCount);
+            // timer.Tick += Timer_Tick;
+            // timer.Start();
             TextBlock.Text = Story[random.Next(0, Story.Length)];
             Textbox.Text= null;
             _correct = 0;
             _incorrect = 0;
+            lblCorrect.Content = "Correct " + _correct;
+            lblIncorrect.Content = "Incorrect " + _incorrect;
+            
+            
+            timer.Tick += new EventHandler(Timer_Tick);
+            timer.Interval = TimeSpan.FromSeconds(1);// TimeSpan.FromSeconds(startCount);
+            timer.Start();
+
+
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            if (startCount >= minCount)
+            {
+                //   TxtInterval.Text = DateTime.Now.Second.ToString();
+
+                TxtInterval.Text = startCount.ToString();
+                startCount--;
+
+                // TxtInterval.Text = timer.Interval.Seconds.ToString();
+            }
+            else
+            {
+                timer.Stop();
+                MessageBox.Show($"User typed {_correct} correct words and {_incorrect} incorrect words");
+
+                Application.Current.Shutdown();
+            }
         }
 
         private void StopClicked(object sender, RoutedEventArgs e)
         {
-            
+            timer.Stop();
+
+            MessageBox.Show($"{_fullName} typed {_correct} correct words and {_incorrect} incorrect words");
+            Application.Current.Shutdown();
+
         }
-       
+
+        private async void nameBoxPressed(object sender, MouseButtonEventArgs e)
+        {
+            fullName.Text = null;
+
+            _fullName = Console.ReadLine();
+
+
+
+        }
     }
 }
